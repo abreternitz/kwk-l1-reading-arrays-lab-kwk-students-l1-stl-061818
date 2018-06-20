3ac7f5ce4470894713ae5d77d430e352ac9964ec
abreternitz <aliceburian@gmail.com>
2018-06-20 15:07:11 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -1,13 +1,9 @@
 disney = ["Mulan","Lilo","Moana","Merida","Rapunzel"]
-puts disney
 
 ages =[17,16,15,13]
-puts ages
 
 books = ["Everything, everything",
           "Ready Player One",
           "Red Queen",
           "Carry On"]
-puts books
 
-puts books[0].upcase
