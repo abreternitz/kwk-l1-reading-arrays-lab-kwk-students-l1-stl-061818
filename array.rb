74f7b46a7ed4098b8fd18ab9e06cd8b89d0bf8ec
abreternitz <aliceburian@gmail.com>
2018-06-20 14:54:19 +0000

    automatically backed up by learn

--- array.rb
+++ array.rb
@@ -1,10 +1,9 @@
-# student_one ="caroline"
-# student_two="millie"
-# student_three="sam"
-# student_four="sonali"
-
-# puts "in this class we have a student named sonali #{student_four}"
-
-friends = ["Ross","Chandler","Joey","Monica","Phoebe","Rachel"]
+friends = ["Ross",
+          "Chandler",
+          "Joey",
+          "Monica",
+          "Phoebe",
+          "Rachel"
+          ]
     
-puts friends[0]
+puts friends
